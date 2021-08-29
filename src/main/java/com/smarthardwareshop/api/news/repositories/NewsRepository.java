aec44e452275f0e64b1477bb8f825bd800ecbfcd
Saulo Soares de Toledo <saulotoledo@gmail.com>
2021-08-29 02:30:58 +0200

    fix(doc): fix news documentation strings

--- src/main/java/com/smarthardwareshop/api/news/repositories/NewsRepository.java
+++ src/main/java/com/smarthardwareshop/api/news/repositories/NewsRepository.java
@@ -7,25 +7,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * Repository for products.
+ * Repository for news.
  */
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
 
     /**
-     * Returns a page of products.
+     * Returns a page of news.
      *
      * @param pageable Object containing pagination information.
-     * @return A page of products.
+     * @return A page of news.
      */
     Page<News> findAll(Pageable pageable);
 
     /**
-     * Returns a page of products according to a specific filter.
+     * Returns a page of news according to a specific filter.
      *
      * @param filter The value used to filter the results.
      * @param pageable Object containing pagination information.
-     * @return A page of products according to the informed filter.
+     * @return A page of news according to the informed filter.
      */
     Page<News> findByTitleContaining(String filter, Pageable pageable);
 }
